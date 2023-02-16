49186d61dbf3788989d1cad832c36a360a4cf319
Karen Tabatini <tabatini.karen@gmail.com>
2023-02-15 23:11:12 -0300

    cookie e logout

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 const express = require('express');
-const passport = require('passport');
-const { googleClientId } = require('./config/keys');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
+mongoose.set('strictQuery', true);
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: 'auth/google/callback'    
-    },  (accessToken, refreshToken, profile, done) =>{
-        console.log('access token:',accessToken);
-        console.log('refres token:',refreshToken);
-        console.log('profile:',profile);
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
 );
 
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-    scope: ['profile', 'email']
-    })
-);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get(
-    '/auth/google/callback',
-    passport.authenticate('google')
-)
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
