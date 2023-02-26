3b665bb77b3cadf135529946ab3e2dc8d9b8079d
Karen Tabatini <tabatini.karen@gmail.com>
2023-02-26 16:09:22 -0300

    Login/Logout flow funcionando

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,9 +16,10 @@ module.exports = app => {
         }
     )
 
-    app.get('/api/logout', (req, res) => {
-        req.logout();
-        res.send(req.user);
+    app.get('/api/logout',
+     (req, res) => {
+        req.logout(),
+        res.redirect("/");
     })
 
     app.get('/api/current_user', (req, res) => {
