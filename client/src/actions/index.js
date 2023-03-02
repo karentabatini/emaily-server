1e28328fd90423a50917b95368fc558133aad9b2
Karen Tabatini <tabatini.karen@gmail.com>
2023-03-01 22:06:37 -0300

    sync local em prod

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchUser = () => async dispatch => {
     const result = await axios.get('/api/current_user');
     return dispatch({ type: FETCH_USER, payload: result.data});
 };
+
+export const handleToken = (token) => async dispatch => {
+    const result = await axios.post('/api/stripe', token);
+    return dispatch({ type: FETCH_USER, payload: result.data});
+}
---
+++ client/src/config.js
@@ -0,0 +1,3 @@
+export const config = {
+    stripeKey: "pk_test_51Mfq3NBMGDFZDqZ6pLDQALjExuYFtNm3P6XmVQUxiEZAFeyWToeVlqkk7qXYcBGYyTlgkRnYVJ7hCqOruNAZT5gT00lucyUxRe"
+};
