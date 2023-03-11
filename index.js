5e94f01da2cf41638561cc033b3028e0d764fc7f
Karen Tabatini <tabatini.karen@gmail.com>
2023-03-11 18:42:26 -0300

    cobrança completa - atualiza creditos no front

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
@@ -10,6 +11,12 @@ mongoose.set('strictQuery', true);
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    bodyParser.json()
+);
+
+
 
 app.use(
     cookieSession({
