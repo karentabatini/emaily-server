463b22ba4128ebc853e165c7b8f51089a12ade75
Karen Tabatini <tabatini.karen@gmail.com>
2023-03-11 19:26:12 -0300

    Inicio model Survey

---
+++ models/Survey.js
@@ -0,0 +1,10 @@
+const Schema = mongoose.Schema;
+
+const surveySchema = new Schema({
+    title: String,
+    body: String,
+    subject: String,
+    recipients: [String]
+});
+
+mongoose.model('surveys',surveySchema);
